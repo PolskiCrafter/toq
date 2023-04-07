e8c81aba8973f1ad927638e90fbfcd4581637a44
PolskiCrafter <jahol.studio@onet.eu>
2023-04-07 17:30:37 +0200

    Start

---
+++ _dev_files/toq2v/common/defines/ToQ_defines.lua
@@ -0,0 +1,1 @@
+NDefines.NGame.START_DATE = "1090.1.1"
---
+++ _dev_files/toq2v/common/defines/ToQ_graphics.lua
@@ -0,0 +1,7 @@
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 2048	--default 256
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512 	--default 64
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10 	--default 10
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196 	--default 4096
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41 	--default 41
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000 	--default 8192
+
